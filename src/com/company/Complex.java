793f36bc66f6c0e0ff22221f0c5a2404a50c39c2
Ryan Gutierrez <ryangutierrez@billericak12.com>
2020-02-07 12:28:40 -0500

    stinky poopy is smelly

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -1,17 +1,39 @@
 package com.company;
 
 public class Complex {
-    double one;
-    double two;
+    double far;
+    double rar;
 
     public Complex(double a){
-        one = a;
-        two = 0.0;
+        far = a;
+        rar = 0.0;
     }
     public Complex(double a, double b){
-        one = a;
-        two = b;
+        far = a;
+        rar = b;
 
     }
-//    public
+    public double abs (){
+        double result = Math.sqrt((far*far)+ (rar*rar));
+        return result;
+    }
+    public Complex add (Complex other){
+        double addfart = far + other.far;
+        double addrart = rar + other.rar;
+        return new Complex(addfart, addrart);
+    }
+    public String toString(){
+        String result = far + "+" + rar + "i";
+        return result;
+    }
+    public static void main(String[] args){
+        Complex p =  new Complex(7.2, 18.5);
+        System.out.println(p.abs());
+        Complex s = new Complex(9.0,6.0);
+        System.out.println(s.add(p));
+        System.out.println(p);
+        System.out.println(s);
+
+    }
+
 }
--- src/com/company/SnackBar.java
+++ src/com/company/SnackBar.java
@@ -71,7 +71,7 @@ public class SnackBar extends JFrame
                     "Login failed", "Service", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
 
     public static void main(String[] args)
     {
